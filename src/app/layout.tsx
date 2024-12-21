10358482e8369968ce90f7f593d77de6cc9b7dc5
adam0x7 <0xuncleboo@gmail.com>
2024-12-21 00:44:44 -0500

    stuff

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Baos.fun - Coming to Berachain in Q5',
+  description: 'Get whitelisted for Baos.fun on Berachain',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${inter.className} min-h-screen bg-black`}>{children}</body>
+    </html>
+  )
+} 
